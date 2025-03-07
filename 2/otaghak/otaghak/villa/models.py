90e8716464a0b7f44e5b5ad34d42e8ff759e8668
SaeedRz96 <saeedramezani75@gmail.com>
2025-03-07 10:52:12 +0330

    models added

--- 2/otaghak/otaghak/villa/models.py
+++ 2/otaghak/otaghak/villa/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Seller(models.Model):
+    name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=20)
+    national_id = models.CharField(max_length=11, unique=True)
+    email = models.EmailField(null=True, blank=True) #optional
+
+
+class Place(models.Model):
+    title = models.CharField(max_length=100)
+    address = models.TextField()
+    price = models.IntegerField()
+    is_valid = models.BooleanField(default=True)
+    datetime = models.DateTimeField()
+    added_datetime = models.DateTimeField(auto_now_add=True)
+    seller = models.ForeignKey(to=Seller, on_delete=models.CASCADE)
