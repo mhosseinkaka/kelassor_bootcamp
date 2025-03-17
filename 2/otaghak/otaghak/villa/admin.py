01df36edc28b0fe58d0cb12f145e5189c7692596
Saeed Ramezani <saeedramezani75@gmail.com>
2025-03-17 15:08:48 +0330

    update admin.py

--- 2/otaghak/otaghak/villa/admin.py
+++ 2/otaghak/otaghak/villa/admin.py
@@ -1,5 +1,16 @@
-from django.contrib import admin
+from django.contrib.admin import ModelAdmin, register
 from villa.models import Seller, Place
 
-admin.site.register(Seller)
-admin.site.register(Place)
+
+@register(Seller)
+class SellerAdmin(ModelAdmin):
+    list_display = ['name', 'phone']
+    search_fields = ['national_id']
+
+
+@register(Place)
+class PlaceAdmin(ModelAdmin):
+    list_display = ['title', 'price', 'is_valid']
+    search_fields = ['title', 'address']
+    ist_filter = ["is_valid"]
+    autocomplete_fields = ['seller']
