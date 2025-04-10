8c9bccb6acf2ff43d0a6ef7e7d09a321a74a78c9
SaeedRz96 <saeedramezani75@gmail.com>
2025-04-10 14:14:36 +0330

    simple jwt

--- 2/otaghak/otaghak/otaghak/urls.py
+++ 2/otaghak/otaghak/otaghak/urls.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('villa/', include('villa.urls')),
-    path('foreign-villa/', include('foreign_villa.urls'))
+    path('foreign-villa/', include('foreign_villa.urls')),
+    path('login', TokenObtainPairView.as_view()),
+    path('refresh', TokenRefreshView.as_view()),
+    path('verify', TokenVerifyView.as_view())
 ]
